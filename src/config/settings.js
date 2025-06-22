370a715221c3dc4f16ed71b0250ad2870791bfe0
noodleman1991 <149662590+noodleman1991@users.noreply.github.com>
2025-06-22 17:29:50 +0200

    updates

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,6 +1,6 @@
 export default {
-	title: `הפרוזדור | רומן מדע בדיוני ישראלי מאת מאיר לוקשינסקי`,
-	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
+	title: `הפרוזדור`,
+	description: `רומן מדע בדיוני ישראלי מאת מאיר לוקשינסקי`,
 	url: `https://prozdor.co.il`, // No trailing slash!
 	name: `מאיר לוקשינסקי`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: false,
